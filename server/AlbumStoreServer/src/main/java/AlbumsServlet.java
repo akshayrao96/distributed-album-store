8b4d65c29f301517d8453ac5611ff874159939e3
Akshay <akshayrao196@gmail.com>
2023-10-11 00:51:01 -0700

    chore: cleaned server, maven dependencies, packages, code structure

--- server/AlbumStoreServer/src/main/java/AlbumsServlet.java
+++ server/AlbumStoreServer/src/main/java/AlbumsServlet.java
@@ -1,7 +1,7 @@
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
@@ -25,14 +25,8 @@ public class AlbumsServlet extends HttpServlet {
 
     Albums example = new Albums("Sex Pistols", "Never Mind The Bollocks!", "1977");
 
-    if (urlPath == null || urlPath.isEmpty()) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-      response.getWriter().write("missing url parameters!");
-      return;
-    }
-
     String[] urlParts = urlPath.split("/");
-    if (!isUrlValidGet(urlParts)) {
+    if (urlParts.length != 2) {
       response.setStatus(HttpServletResponse.SC_NOT_FOUND);
     } else {
       response.getOutputStream().print(gson.toJson(example));
@@ -40,70 +34,24 @@ public class AlbumsServlet extends HttpServlet {
     }
   }
 
-  private boolean isUrlValidGet(String[] urlPath) {
-    return urlPath.length == 2 && isValidID(urlPath[1]);
-  }
-
-  private boolean isValidID(String s) {
-    try {
-      int id = Integer.parseInt(s);
-      return id > 0;
-    } catch (NumberFormatException e) {
-      return false;
-    }
-  }
 
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
     response.setContentType("application/json");
+    StringBuilder imgSize = new StringBuilder();
+    Part part = request.getPart("image");
+    long size = part.getSize();
+    imgSize.append(size).append("KB");
 
-    String urlPath = request.getPathInfo();
+    JsonObject jsonObject = new JsonObject();
+    jsonObject.addProperty("albumID", "1");
+    jsonObject.addProperty("imageSize", imgSize.toString());
 
-    if (urlPath != null) {
-      String[] urlParts = urlPath.split("/");
-      if (!isUrlValidPost(urlParts)) {
-        response.getWriter().write("Invalid parameters given\n");
-        response.getWriter().write("usage: /albums");
-        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-      }
-
-    } else {
-
-      Gson gson = new Gson();
-      String data = request.getParameter("profile");
-
-//      Albums album = gson.fromJson(data, Albums.class);
-//      JsonObject jsonObject = new Gson().toJsonTree(album).getAsJsonObject();
-
-
-//      String artist = jsonObject.get("artist").getAsString();
-//      String title = jsonObject.get("title").getAsString();
-//      String year = jsonObject.get("year").getAsString();
-//      Albums album = new Albums(artist, title, year);
-
-      String imgSize = "0 KB";
-
-      Part part = request.getPart("image");
-
-      if (part != null) {
-        imgSize = part.getSize() + " KB";
-      } else {
-        response.getOutputStream().print("null");
-      }
-
-      Map<String, String> responseMap = new HashMap<>();
-      responseMap.put("albumID", "1");
-      responseMap.put("imageSize", imgSize);
-
-      response.getOutputStream().print(gson.toJson(responseMap));
-
+    try (PrintWriter out = response.getWriter()) {
+      out.write(jsonObject.toString());
     }
-  }
-
-  private boolean isUrlValidPost(String[] urlPath) {
-    return urlPath == null || urlPath.length == 0;
   }
 }
 
