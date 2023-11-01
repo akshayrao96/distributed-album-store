76421404619d4dee29f8429ccb9b7eb4559b67f4
Akshay <akshayrao196@gmail.com>
2023-10-31 23:27:47 -0700

    chore: rearranged post and get for readability/ordering

--- server/AlbumStoreServer/src/main/java/AlbumsServlet.java
+++ server/AlbumStoreServer/src/main/java/AlbumsServlet.java
@@ -34,7 +34,6 @@ public class AlbumsServlet extends HttpServlet {
     }
   }
 
-
   @Override
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
